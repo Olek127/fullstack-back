082f8583e6da7fd6b9082ebada80627be075acf1
Oleksandr <olek.olefirenko7@gmail.com>
2022-12-16 12:06:37 +0100

    Archivos backend

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, redirect
+import persistencia
+
+#from flask import request
+
+app = Flask(__name__)
+
+@app.route("/pizza", methods=['POST'])
+def pizza_post():
+    nombre = request.form.get("nombre") 
+    apellidos = request.form.get("apellidos")
+    print("Nombre: " + nombre)
+    print("Apellidos: " + apellidos)
+    persistencia.guardar_pedido(nombre, apellidos)
+    return redirect("http://localhost/solicita_pedido.html", code=302)
+
+
