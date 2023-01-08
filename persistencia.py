0fd2f8cce6bd2e17dcfb877e3c72c6f23ce851e7
Oleksandr <olek.olefirenko7@gmail.com>
2023-01-08 17:31:56 +0100

    Revisión fallo test
    Revisión fallo test

--- persistencia.py
+++ persistencia.py
@@ -4,5 +4,5 @@ Persistencia
 def guardar_pedido(nombre, apellidos):
     """ Guarda el pedido con el nombre y apellidos """
     with open("pedidos.txt", "a", encoding="utf-8") as file:
-        file.write(nombre + " " + apellidos + "\n")
+        file.write("-" + nombre + " " + apellidos + "\n")
         file.close()
