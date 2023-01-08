85c4b318247e6494a7403880b45ef1030da8bb39
Oleksandr <olek.olefirenko7@gmail.com>
2023-01-08 17:28:47 +0100

    Cambios caldiad app.py
    Cambios caldiad app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-""" 
-App Pizza 
+"""
+App Pizza
 """
 from flask import Flask, request, redirect
 import persistencia
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route("/pizza", methods=['POST'])
 def pizza_post():
     """ Recoge el nombre y apellido, y lo envia a persistencia """
-    nombre = request.form.get("nombre") 
+    nombre = request.form.get("nombre")
     apellidos = request.form.get("apellidos")
     print("Nombre: " + nombre)
     print("Apellidos: " + apellidos)
