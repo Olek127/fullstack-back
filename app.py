a92c2ee6ca2f347daca76cbf040e0e211c103134
Oleksandr <olek.olefirenko7@gmail.com>
2023-01-08 17:26:32 +0100

    Cambios app y persistencia (Pylint)
    Cambios app y persistencia (Pylint)

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+""" 
+App Pizza 
+"""
 from flask import Flask, request, redirect
 import persistencia
 
@@ -7,11 +10,10 @@ app = Flask(__name__)
 
 @app.route("/pizza", methods=['POST'])
 def pizza_post():
+    """ Recoge el nombre y apellido, y lo envia a persistencia """
     nombre = request.form.get("nombre") 
     apellidos = request.form.get("apellidos")
     print("Nombre: " + nombre)
     print("Apellidos: " + apellidos)
     persistencia.guardar_pedido(nombre, apellidos)
     return redirect("http://localhost/solicita_pedido.html", code=302)
-
-
--- persistencia.py
+++ persistencia.py
@@ -1,4 +1,8 @@
+"""
+Persistencia
+"""
 def guardar_pedido(nombre, apellidos):
+    """ Guarda el pedido con el nombre y apellidos """
     with open("pedidos.txt", "a", encoding="utf-8") as file:
         file.write(nombre + " " + apellidos + "\n")
         file.close()
